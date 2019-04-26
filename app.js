a5a77a41e0b02e9e852fccf8bf058ce274a91a6e
Trevor <trevor.blixt@students.dominican.edu>
2019-04-26 00:31:11 -0700

    final

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 // app.js
 
 // Require Libraries
+var http = require('http');
 var express = require('express');
 var exphbs  = require('express-handlebars');
 var giphy = require('giphy-api')();
 // App Setup
 var app = express();
 
+app.use(express.static('public'));
+
 // Middleware
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-
-app.get('/hello-gif', function (req, res) {
-  var gifUrl = 'http://media2.giphy.com/media/gYBVM1igrlzH2/giphy.gif'
-  res.render('hello-gif', {gifUrl: gifUrl})
-})
 
 // Routes
 app.get('/greetings/:name', function (req, res) {
@@ -27,6 +25,45 @@ app.get('/', (req, res) => {
   console.log(req.query) // => "{ term: hey" }
   res.render('home')
 })
+
+// REQUIRE HTTP MODULE
+
+app.get('/', (req, res) => {
+  console.log(req.query.term)
+  const queryString = req.query.term;
+  // ENCODE THE QUERY STRING TO REMOVE WHITE SPACES AND RESTRICTED CHARACTERS
+  const term = encodeURIComponent(queryString);
+  // PUT THE SEARCH TERM INTO THE GIPHY API SEARCH URL
+  const url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=dc6zaTOxFJmzC'
+
+  http.get(url, (response) => {
+    // SET ENCODING OF RESPONSE TO UTF8
+    response.setEncoding('utf8');
+
+    var body = '';
+
+    response.on('data', (d) => {
+      // CONTINUOUSLY UPDATE STREAM WITH DATA FROM GIPHY
+      body += d;
+    });
+
+    response.on('end', () => {
+      // WHEN DATA IS FULLY RECEIVED PARSE INTO JSON
+      const gifs = JSON.parse(body).data
+      // RENDER THE HOME TEMPLATE AND PASS THE GIF DATA IN TO THE TEMPLATE
+      res.render('home', { gifs });
+    });
+  });
+});
+
+// Routes
+
+app.get('/hello-gif', function (req, res) {
+  var gifUrl = 'http://media2.giphy.com/media/gYBVM1igrlzH2/giphy.gif'
+  res.render('hello-gif', {gifUrl: gifUrl})
+})
+
+// GIPHY API Route
 
 app.get('/', (req, res) => {
   giphy.search(req.query.term, (err, response) => {
